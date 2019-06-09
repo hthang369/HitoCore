08f6b0b590a6b111032e7bb750bd67043f47ffba
Thang <thangtran369@hotmail.com.vn>
2019-06-09 21:28:55 +0700

    add

---
+++ App4/App4/TestPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HitoAppCore
+{
+	public class TestPage : ContentPage
+	{
+		public TestPage ()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Welcome to Xamarin.Forms!" }
+				}
+			};
+            LoadControl();
+
+        }
+        public void LoadControl()
+        {
+            try
+            {
+                //Xamarin.Forms.DataGrid.DataGrid grid = new Xamarin.Forms.DataGrid.DataGrid();
+                //grid.Columns.Add(new Xamarin.Forms.DataGrid.DataGridColumn { FieldName = "no", Caption = "no" });
+                //grid.Columns.Add(new Xamarin.Forms.DataGrid.DataGridColumn { FieldName = "name", Caption = "name" });
+                //(Content as StackLayout).Children.Add(grid);
+
+                Xamarin.Forms.Calendar.Calendar calendar = new Xamarin.Forms.Calendar.Calendar();
+                calendar.ShowInBetweenMonthLabels = true;
+                calendar.ShowNumberOfWeek = true;
+                calendar.EnableTitleMonthYearView = true;
+                calendar.WeekdaysShow = true;
+                (Content as StackLayout).Children.Add(calendar);
+
+                DatePicker picker = new DatePicker();
+                (Content as StackLayout).Children.Add(picker);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+	}
+}
