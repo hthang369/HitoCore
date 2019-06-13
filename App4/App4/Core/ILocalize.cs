9ddb8623f0c4b07db23e4c2bce84a3b27a853f7a
Thang <thangtran369@hotmail.com.vn>
2019-06-13 20:49:50 +0700

    add

---
+++ App4/App4/Core/ILocalize.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace HitoAppCore.Core.Localization
+{
+    internal interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+        void SetLocale(CultureInfo ci);
+    }
+}
---
+++ App4/App4/Core/TranslateExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HitoAppCore.Core.Localization
+{
+    [ContentProperty("Text")]
+    class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci = null;
+        public string Text { get; set; }
+        static readonly Lazy<ResourceLang> ResMgr = new Lazy<ResourceLang>(() => new ResourceLang());
+        public TranslateExtension()
+        {
+            if(Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            }
+        }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return string.Empty;
+
+            var translation = ResMgr.Value.GetString(Text, ci);
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    string.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
+                    "Text");
+#else
+                translation = Text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
